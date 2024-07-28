0e201ea9c8e90988e706f6df111224cc65c8e12a
Jeú Chaves <jeuchaveslima@gmail.com>
2024-07-28 00:35:59 -0400

    feat: loads select profile dialog on suggestions page when no profileid is stored locally

--- src/pages/filmes/Sugestoes.tsx
+++ src/pages/filmes/Sugestoes.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { LayoutBaseDePagina } from "../../shared/layouts"
 import { FilmesService, IListagemFilme } from "../../shared/services/api/filmes/FilmesService";
+import { LOCAL_STORAGE_PERFIL_ID } from "../../shared/services/api/Perfis/PerfisService";
+import { SelecionarPerfil } from "../../shared/components";
 
 export const Sugestoes = () => {
 
@@ -8,8 +10,18 @@ export const Sugestoes = () => {
     const [totalCount, setTotalCount] = useState(0);
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
+    const [isDialogProfilesOpen, setIsDialogProfilesOpen] = useState<boolean>(false);
+
     useEffect(() => {
         setIsLoading(true);
+        
+        const perfilId = localStorage.getItem(LOCAL_STORAGE_PERFIL_ID);
+        if (!perfilId) {
+            setIsDialogProfilesOpen(true);
+            setIsLoading(false);
+            return;
+        }
+
         FilmesService.getSuggested()
             .then((result) => {
                 setIsLoading(false);
@@ -22,13 +34,14 @@ export const Sugestoes = () => {
         });
     }, []);
 
+    const handleCloseDialogProfiles = () => {
+        setIsDialogProfilesOpen(false);
+    }
+
     return (
         <LayoutBaseDePagina titulo="Filmes sugeridos">
-            { !isLoading && (
-                <>
-                    Não está carregando
-                </>
-            )}
+            <SelecionarPerfil open={isDialogProfilesOpen} onClose={handleCloseDialogProfiles} />
+            Página
         </LayoutBaseDePagina>
     )
 }
